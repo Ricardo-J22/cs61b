d313f9fba92bd0b6bf7a4d9f79ba35da2e7302c4
Ricardo-J22 <1543400884.com>
2021-01-17 16:03:27 +0800

    lab 02

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,9 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null) {
+            return null;
+        }
         IntList head = A;
         while (A.rest != null){
             A = A.rest;
@@ -96,6 +99,9 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null) {
+            return null;
+        }
         IntList C = new IntList(A.first, null);
         IntList head = C;
         while(A.rest != null){
